f75852cfa9e65236a4c376260b1355d31bbe4b6c
flyingsl0ths <randomemail32110@gmail.com>
2022-06-14 23:39:52 -0500

    Implement database model

--- server/Note.hs
+++ server/Note.hs
@@ -1,1 +1,35 @@
-module Note () where
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE DataKinds                  #-}
+
+module Note
+  ( Note(..)
+  , migrateAll
+  ) where
+
+import           Database.Persist.TH            ( mkMigrate
+                                                , mkPersist
+                                                , persistLowerCase
+                                                , share
+                                                , sqlSettings
+                                                )
+
+import           Data.Text                      ( Text )
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Note json
+  text Text
+  author Text
+  date Text
+  deriving Show
+|]
