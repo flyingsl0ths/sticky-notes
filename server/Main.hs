3e68225f426db2153f35a8a61eccb8a5bba66c16
flyingsl0ths <randomemail32110@gmail.com>
2022-06-14 23:39:18 -0500

    Refactor Main::setup (formerly Main::app)

--- server/Main.hs
+++ server/Main.hs
@@ -1,30 +1,81 @@
-module Main
-  ( main
-  , app
-  ) where
+module Main where
 
-import           ServerConfig                   ( ServerConfig(debugMode, port)
+import           ServerConfig                   ( ServerConfig
+                                                  ( dbName
+                                                  , dbPoolSize
+                                                  , debugMode
+                                                  , domainName
+                                                  , logFile
+                                                  , port
+                                                  )
                                                 , getServerConfig
                                                 )
 
-import           Routes                         ( routes )
+import           Routes                         ( ServerContext
+                                                  ( ServerContextC
+                                                  , domain
+                                                  )
+                                                , routes
+                                                )
 
 import           Web.Spock                      ( runSpockNoBanner
                                                 , spock
                                                 )
-import           Web.Spock.Config               ( PoolOrConn(PCNoDatabase)
+
+import           Web.Spock.Config               ( PoolOrConn(PCPool)
                                                 , defaultSpockCfg
                                                 )
+
+import           Control.Monad.Logger           ( logInfoN
+                                                , runFileLoggingT
+                                                , runNoLoggingT
+                                                )
+
+import           Database.Persist.Sqlite        ( createSqlitePool
+                                                , runMigration
+                                                , runSqlPool
+                                                )
+
+import           Data.Text                      ( pack )
+
+import           Note                           ( migrateAll )
 
 
 main :: IO ()
 main = do
-  (serverConfig, app') <- app
-  print serverConfig
+  (serverConfig, app') <- setup
+
   runSpockNoBanner (port serverConfig) app'
 
-app = do
+setup = do
   serverConfig <- getServerConfig
-  spockCfg     <- defaultSpockCfg () PCNoDatabase ()
-  let debug = debugMode serverConfig
-  return (serverConfig, spock spockCfg $ routes debug)
+
+  let logFile' = logFile serverConfig
+
+  pool <- runFileLoggingT
+    logFile'
+    (do
+      logInfoN (pack $ show serverConfig)
+
+      pool <- do
+        logInfoN (pack "Initializing database")
+        createSqlitePool (pack $ dbName serverConfig) (dbPoolSize serverConfig)
+
+      logInfoN $ pack "Running migrations"
+      runSqlPool
+        (do
+          runMigration migrateAll
+        )
+        pool
+
+      return pool
+    )
+
+  spockCfg <- defaultSpockCfg () (PCPool pool) ()
+
+  let ctx = ServerContextC (domainName serverConfig)
+                           (debugMode serverConfig)
+                           logFile'
+
+  return (serverConfig, spock spockCfg $ routes ctx)
+
