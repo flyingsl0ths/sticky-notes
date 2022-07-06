90544ed29af3a094f2bb4e23ebbb70649cf81ac0
flyingsl0ths <randomemail32110@gmail.com>
2022-07-05 22:00:42 -0500

    Update config

--- hash.py
+++ hash.py
@@ -1,6 +1,6 @@
 from sys import argv
 from hashlib import sha256
 
-text: bytes = (argv[1] if len(argv) else "Lorem ipsum dolor sit amet").encode()
+text: bytes = (argv[1] if len(argv) == 2 else "Lorem ipsum dolor sit amet").encode()
 
 print(sha256(text).hexdigest())
