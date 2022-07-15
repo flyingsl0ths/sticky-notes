7d2d933a5b941c2611c7ec7a7b9c2c7043a430e7
flyingsl0ths <randomemail32110@gmail.com>
2022-07-14 23:41:15 -0500

    Update nativeBuildInputs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,26 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   nativeBuildInputs = (with pkgs; [
-    zlib
+    nodejs
+    nodePackages.prettier
+    nodePackages.sass
+
+    cabal-install
+    haskell-language-server
+    haskellPackages.fourmolu
+    haskellPackages.cabal-fmt
+    (haskell.packages.ghc8107.ghcWithPackages (p: [ p.zlib ]))
+    haskellPackages.hlint
+
     nixpkgs-fmt
   ]);
+  # Optional
+  shellHook =
+    let
+      # Primarily used for language servers/formatters when opening project in neovim/vim
+      npmGlobalDirectory = ".npm-global";
+    in
+    ''
+      export PATH="${builtins.getEnv "HOME"}/${npmGlobalDirectory}/bin:$PATH"
+    '';
 }
